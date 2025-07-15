c2ed2dfd2653a391ed2f75e05801ce3c562aa62a
ThiagoClementino <thidf57@gmail.com>
2025-07-15 12:29:57 -0300

    ajueste de segurança

--- src/App.js
+++ src/App.js
@@ -1,165 +1,23 @@
-import { Route, Routes } from "react-router-dom";
-import Dashboard from "./Pages/Dashboard/Dashboard";
-import Membros from "./Pages/Membros/Membros";
-import { useState, useEffect } from "react";
-import Datainfor from "./Contexts/DataInfor";
-import DataApiOne from "./Contexts/DataApiOne";
-import Cadastro from "./Pages/Cadastro/Cadastro";
-import { Financeiro } from "./Pages/Financeiro/Financeiro";
-import Sidebar from "./Pages/Header/Sidebar";
-import MembroMinisterio from "./Pages/Membros/MembroMinisterio";
-import Home from "./Pages/Home/Home";
-import Config from "./Pages/Config/Config";
-import CadMembers from "./Pages/Membros/CadMembers";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js"; // Importante para funcionalidades como Tooltips
+import { AuthProvider } from "./Contexts/AuthContext";
 
-import Login from "./Pages/Login/Login";
-import CreateUser from "./Pages/Users/CreateUser";
-import PrivateRoute from "./Components/PrivateRoute";
+// Seus outros imports e código...
 
-const AppRoutes = () => {
-  const [dados, setDados] = useState([]);
-  const [dadosfinance, setDadosfinance] = useState([]);
-
-  // Rota de consulta todos menbros
-
-  useEffect(() => {
-    fetch("https://api-gestao-igreja-jcod.vercel.app/membros", {
-      method: "GET",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    })
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        if (Array.isArray(data)) {
-          setDados(data);
-        } else {
-          console.error("Dados da API não é um array");
-        }
-      })
-      .catch((error) =>
-        console.error("There was a problem with the fetch operation:", error)
-      );
-  }, []);
-
-  useEffect(() => {
-    fetch("https://api-gestao-igreja-jcod.vercel.app/finance", {
-      method: "GET",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    })
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        if (Array.isArray(data)) {
-          setDadosfinance(data);
-        } else {
-          console.error(`error`);
-        }
-      })
-      .catch((error) =>
-        console.error("There was a problem with the fetch operation:", error)
-      );
-  }, []);
-
+function App() {
   return (
-    <DataApiOne.Provider>
-      <Datainfor.Provider
-        value={{ dadosfinance, setDadosfinance, dados, setDados }}
-      >
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/criarusuario" element={<CreateUser />} />
-          <Route
-            path="/dashboard"
-            element={
-              <PrivateRoute>
-                <Dashboard />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/membros"
-            element={
-              <PrivateRoute>
-                <Membros />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/cadastro"
-            element={
-              <PrivateRoute>
-                <Cadastro />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/financeiro"
-            element={
-              <PrivateRoute>
-                <Financeiro />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/sidebar"
-            element={
-              <PrivateRoute>
-                <Sidebar />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/membro/:id"
-            element={
-              <PrivateRoute>
-                <MembroMinisterio />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/config"
-            element={
-              <PrivateRoute>
-                <Config />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/home"
-            element={
-              <PrivateRoute>
-                <Home />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/cad"
-            element={
-              <PrivateRoute>
-                <CadMembers />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </Datainfor.Provider>
-    </DataApiOne.Provider>
+    <div className="App">
+      <BrowserRouter>
+        <AuthProvider>
+          <AppRoutes />
+        </AuthProvider>
+      </BrowserRouter>
+    </div>
   );
-};
+}
 
-export default AppRoutes;
+export default App;
